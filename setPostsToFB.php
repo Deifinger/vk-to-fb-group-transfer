feb9f4b332251a91fe3895dbf24aca6f21406d78
Deifinger <ruslankostikov@gmail.com>
2017-07-24 23:23:51 +0300

    Update working with attachments

--- setPostsToFB.php
+++ setPostsToFB.php
@@ -4,6 +4,8 @@
  * Date: 7/23/17 8:18 PM
  */
 
+use \VKToFB\Facebook;
+
 require 'config.php';
 
 // is file writable?
@@ -14,47 +16,19 @@ if (!is_readable($config['postsFile']))
 
 session_start();
 
-$scope = array('publish_actions', 'publish_pages', 'manage_pages', 'user_managed_groups');
-$access_token = $config['fb']['access_token'];
-if(isset($_SESSION['fb_access_token']))
-{
-    $access_token = $_SESSION['fb_access_token'];
-}
+$fb = new Facebook(array(
+    'scopes'        => array('publish_actions', 'publish_pages', 'manage_pages', 'user_managed_groups'),
+    'access_token'  => $config['fb']['access_token'],
+    'app_id'        => $config['fb']['app_id'],
+    'app_secret'    => $config['fb']['app_secret'],
+    'default_graph_version' => $config['fb']['api_version'],
+    'callback_url'  => $config['fb']['callback_url']
+));
 
-$options = array(
-    'app_id' => $config['fb']['app_id'],
-    'app_secret' => $config['fb']['app_secret'],
-    'default_graph_version' => $config['fb']['api_version']
-);
-if($access_token !== null)
-{
-    $options['default_access_token'] = $access_token;
-}
-
-// init Facebook object
-$fb = new \Facebook\Facebook($options);
-
-// get login url for exception case
-$helper = $fb->getRedirectLoginHelper();
-$auth_url = $helper->getLoginUrl($config['fb']['callback_url'], $scope);
 
 $code = isset($_REQUEST['code']) ? $_REQUEST['code'] : '';
 // if we have not access token
-if($access_token == null)
-{
-    // and hove not code
-    if(empty($code))
-    {
-        // login as user and back with code
-        header('Location: ' . $auth_url);
-        exit;
-    }
-
-    // if we have a code, we are get access token from code
-    $at_info = $fb->getOAuth2Client()->getAccessTokenFromCode($code, $config['fb']['callback_url']);
-    $_SESSION['fb_access_token'] = $at_info->getValue();
-    $fb->setDefaultAccessToken($at_info);
-}
+$fb->updateAccessTokenIfNeed($code);
 
 // get posts for uploading
 $ch = fopen($config['postsFile'], 'r');
@@ -65,36 +39,74 @@ fclose($ch);
 $posts = json_decode($posts);
 if(empty($posts))
 {
-    throw new Exception('It\'s nothing to post');
+    throw new \Exception('It\'s nothing to post');
 }
-//print_r($posts[0]);
+
+foreach ($posts as $ind => $post)
+{
+    if(isset($post->attachments))
+    {
+        echo $ind . ': ' . $post->attachments[0]->type . '<br>';
+    }
+}
+
+//print_r($posts[2]);
+//exit;
+
+$FBPublisher = new \VKToFB\FacebookPublisher($fb->getFB());
+$FBPost = new \VKToFB\FBPost($posts[4]);
 
 try
 {
-    $fb->post($config['fb']['group_id'].'/photos', array(
-        'url'       => $posts[0]->attachments[0]->photo->photo_130,
-        'message'   => $posts[0]->text
-    ));
+    switch ($FBPost->getType())
+    {
+        case 'photo':
+        case 'album':
+
+            $FBPublisher->postPhoto(
+                $config['fb']['group_id'],
+                $FBPost->getAttachments()[0]->getUrl(),
+                $FBPost->getAttachments()[0]->getCaption(),
+                $FBPost->getText()
+            );
+            break;
+
+        case 'text':
+
+            $FBPublisher->postText(
+                $config['fb']['group_id'],
+                $FBPost->getText()
+            );
+            break;
+
+        case 'link':
+
+            $FBPublisher->postLink(
+                $config['fb']['group_id'],
+                $FBPost->getAttachments()[0]->getUrl(),
+                $FBPost->getText()
+            );
+            break;
+
+        case 'video':
+            break;
+        default:
+            throw new Exception('Unknown post type');
+    }
+
 }
 catch (\Facebook\Exceptions\FacebookAuthenticationException $ex)
 {
     echo 'Error message: ' . $ex->getMessage();
-    // clear session
-    unset($_SESSION['fb_access_token']);
-    header('Location: ' . $auth_url);
-    exit;
+    $fb->forceAuthUser();
 }
 catch (\Facebook\Exceptions\FacebookResponseException $ex)
 {
     echo 'Error message: ' . $ex->getMessage();
-    // clear session
-    unset($_SESSION['fb_access_token']);
-    header('Location: ' . $auth_url);
-    exit;
+    $fb->forceAuthUser();
 }
 catch (Exception $ex)
 {
-    print_r($ex);
     echo 'Error message: ' . $ex->getMessage();
 }
 
